011d8b6c4fd77ba75b6bc6a24256b9b2864e95b9
Akhil Chauhan <akhilnchauhan@gmail.com>
2025-10-25 16:16:52 +0100

    1.05

--- exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
+++ exercises/01-ai-sdk-basics/01.05-stream-text-to-terminal/problem/main.ts
@@ -4,9 +4,9 @@ import { streamText } from 'ai';
 const model = google('gemini-2.0-flash');
 
 const prompt =
-  'Give me the first paragraph of a story about an imaginary planet.';
+  'Give me the first paragraph of a story about an imaginary planet called Earth.';
 
-const stream = TODO; // TODO - stream some text with the model above.
+const stream = streamText({ model, prompt });
 
 for await (const chunk of stream.textStream) {
   process.stdout.write(chunk);
